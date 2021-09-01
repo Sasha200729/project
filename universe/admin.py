202fd64a2836e54dcc2abf2f6b28a81ed6a65313
Sasha200729 <alexanderprozorovsky@gmail.com>
2021-09-01 19:17:49 +0300

    first

---
+++ universe/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from universe.models import (
+    Galaxy,
+    StarSystem,
+    Star,
+    Planet,
+
+)
+# Register your models here.
+@admin.register(Galaxy)
+class GalaxyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(StarSystem)
+class StarSystemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Star)
+class StarAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Planet)
+class PlanetAdmin(admin.ModelAdmin):
+    pass
+
+
